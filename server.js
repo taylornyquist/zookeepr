501be3a9e940d08a12754ad037931c557997932d
Taylor Nyquist <tnyquist@gmail.com>
2020-08-11 03:22:31 -0500

    tested req.param

--- server.js
+++ server.js
@@ -46,6 +46,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id) [0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -53,6 +58,15 @@ app.get('/api/animals', (req, res) => {
     }
     // console.log(req.query)
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
